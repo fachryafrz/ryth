d0e2450cdb14962d7a5bed14a7378af5c3fbf94b
fachryafrz <fachry.dwi.fd.fda@gmail.com>
2023-03-02 15:10:10 +0700

    Update: Added play icon

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -1,9 +1,22 @@
+import { IonIcon } from "@ionic/react";
+import * as Icon from "ionicons/icons";
+
 export default function TrackCard({ item }) {
   return (
     <button id="trackCard" className={`relative rounded-lg overflow-hidden`}>
       <figure className={`aspect-square transition-all`}>
         <img src={item.img_path} alt={item.title} />
       </figure>
+      <div
+        id="playIcon"
+        className={`opacity-0 absolute inset-0 grid place-items-center transition-all`}
+      >
+        <div
+          className={`grid place-items-center p-4 rounded-full bg-gray-800 bg-opacity-70 max-w-fit`}
+        >
+          <IonIcon icon={Icon.play} />
+        </div>
+      </div>
       <div
         id="contents"
         className={`text-left absolute inset-x-0 bottom-0 translate-y-full bg-gray-900 bg-opacity-50 p-2 backdrop-blur transition-all`}
