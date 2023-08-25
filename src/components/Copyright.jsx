658dcc100e964c2292cfba4b0494775fe472ce9d
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2023-08-25 08:45:39 +0700

    Update: Simplify text

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function Copyright() {
   return (
     <div className="flex cursor-default flex-wrap justify-center gap-1 border-b border-neutral-700 bg-neutral-900 p-2 text-sm">
-      Designed by
+      Design by
       <a
         href="https://dribbble.com/bayusasmita"
         target="_blank"
-        className="text-[#ea4c89] transition-all hover:font-medium"
+        className="text-[#ea4c89] font-medium"
       >
         Bayu Sasmita
       </a>
-      & Developed by
+      & Built by
       <a
-        href="https://dribbble.com/fachryafrz"
+        href="https://fachryafrz.vercel.app/"
         target="_blank"
-        className="text-[#ea4c89] transition-all hover:font-medium"
+        className="text-[#ea4c89] font-medium"
       >
         Fachry Dwi Afriza
       </a>
